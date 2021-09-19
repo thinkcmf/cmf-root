86fd118554c5da371477cc7d1007be11ac0f8537
dean <zxxjjforever@163.com>
2021-09-19 23:26:08 +0800

    init

---
+++ src/RootDirPlugin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace cmf\composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class RootDirPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        echo "RootDirPlugin activate\n";
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+
+    }
+}
