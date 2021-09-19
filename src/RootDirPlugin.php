34106fd53bfa315558b53d90d27ba3e2609ecc4c
dean <zxxjjforever@163.com>
2021-09-19 23:53:39 +0800

    init

--- src/RootDirPlugin.php
+++ src/RootDirPlugin.php
@@ -10,8 +10,9 @@ class RootDirPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
+        print_r($composer);
 
-        print_r($composer->getPackage()->getIncludePaths());
+        print_r($composer->getInstallationManager()->getInstallPath($composer->getPackage()));
 
         echo "RootDirPlugin activate\n";
     }
