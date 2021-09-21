f126eadc8a37544e08865b4272520fe021f3efeb
dean <zxxjjforever@163.com>
2021-09-21 16:29:40 +0800

    init

--- src/RootDirPlugin.php
+++ src/RootDirPlugin.php
@@ -10,19 +10,40 @@ class RootDirPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
-        print_r($composer->getPackage());
-        echo "\n";
-        $vendorDir = $composer->getConfig()->get('vendor-dir');
-
+        $vendorDir  = $composer->getConfig()->get('vendor-dir');
         $cmfRootDir = dirname($vendorDir) . DIRECTORY_SEPARATOR;
+        $rootDir    = $vendorDir . DIRECTORY_SEPARATOR . 'thinkcmf' . DIRECTORY_SEPARATOR . 'cmf-root' . DIRECTORY_SEPARATOR . 'root' . DIRECTORY_SEPARATOR;
+        $this->copyDir($rootDir, $cmfRootDir);
 
-        $rootDir = $vendorDir . DIRECTORY_SEPARATOR . 'thinkcmf' . DIRECTORY_SEPARATOR . 'cmf-root' . DIRECTORY_SEPARATOR . 'root' . DIRECTORY_SEPARATOR;
+        echo "copy done\n";
+    }
 
-        $content = file_get_contents("{$rootDir}test.txt");
-
-        file_put_contents("{$cmfRootDir}test.txt", $content);
-
-        echo "\nRootDirPlugin activate 1\n";
+    private function copyDir($strSrcDir, $strDstDir)
+    {
+        $dir = opendir($strSrcDir);
+        if (!$dir) {
+            return false;
+        }
+        if (!is_dir($strDstDir)) {
+            if (!mkdir($strDstDir)) {
+                return false;
+            }
+        }
+        while (false !== ($file = readdir($dir))) {
+            if (($file != '.') && ($file != '..')) {
+                if (is_dir($strSrcDir . DIRECTORY_SEPARATOR . $file)) {
+                    if (!$this->copyDir($strSrcDir . DIRECTORY_SEPARATOR . $file, $strDstDir . DIRECTORY_SEPARATOR . $file)) {
+                        return false;
+                    }
+                } else {
+                    if (!copy($strSrcDir . DIRECTORY_SEPARATOR . $file, $strDstDir . DIRECTORY_SEPARATOR . $file)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        closedir($dir);
+        return true;
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
